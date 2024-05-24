a497a84cdd5b2b921302f9a2a54707cec8c784ae
zima <yeenimal@pm.me>
2024-05-24 12:01:32 -0700

    ✨ Add ability to use custom image versions
    - Added variables to separate image name and version for better readability.
    - Updated docker run command to use the separated image name and version variables.

--- boxer.sh
+++ boxer.sh
@@ -1,3 +1,7 @@
 boxer() {
-  docker run -it --rm --mount type=bind,source="$(pwd)",target=/host --hostname boxer-"$1" "$1":latest "${@:2}"
+  # setup vars
+  IMAGE=$1
+  IFS=':' read -r IMAGE_NAME IMAGE_VERSION <<< "$IMAGE"
+
+  docker run -it --rm --mount type=bind,source="$(pwd)",target=/host --hostname "$0"-"$IMAGE_NAME" "$IMAGE_NAME":"$IMAGE_VERSION" ${@:2}
 }
