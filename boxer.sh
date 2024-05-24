c131ed87ab7e938b9d13308791972f64294b6b06
zima <yeenimal@pm.me>
2024-05-24 12:44:42 -0700

    🛠️ Fix empty IMAGE_VERSION
    Add a condition to check if the IMAGE_VERSION is empty before running the docker command. If it's empty, it runs the docker command without specifying an image version. This change was made to prevent errors when no image version is provided.

--- boxer.sh
+++ boxer.sh
@@ -3,5 +3,10 @@ boxer() {
   IMAGE=$1
   IFS=':' read -r IMAGE_NAME IMAGE_VERSION <<< "$IMAGE"
 
-  docker run -it --rm --mount type=bind,source="$(pwd)",target=/host --hostname "$0"-"$IMAGE_NAME" "$IMAGE_NAME":"$IMAGE_VERSION" ${@:2}
+  if [ -z "${IMAGE_VERSION}" ]; then
+    echo empty
+    docker run -it --rm --mount type=bind,source="$(pwd)",target=/host --hostname "$0"-"$IMAGE_NAME" "$IMAGE_NAME" ${@:2}
+  else
+    docker run -it --rm --mount type=bind,source="$(pwd)",target=/host --hostname "$0"-"$IMAGE_NAME" "$IMAGE_NAME":"$IMAGE_VERSION" ${@:2}
+  fi
 }
