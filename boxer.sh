a621cbc8374c487cf4cc192477b1d5b6e5636700
zima <yeenimal@pm.me>
2025-01-30 17:20:01 -0700

    Thematic logging and some minor code refactoring

--- boxer.sh
+++ boxer.sh
@@ -1,6 +1,6 @@
 boxer() {
   # setup vars
-  BOXER_VERSION=25.1.9c
+  BOXER_VERSION=25.1.27
   BACKEND="docker" #you can change this to podman or nerdctl if you wish
   BOXER_NAME=$0
   IMAGE=$1
@@ -11,33 +11,38 @@ boxer() {
 
   # check if container exists and remove it if so
   if $BACKEND inspect "$BOXER_NAME-$IMAGE_NAME" > /dev/null 2>&1; then
+    echo "Sweeping the ring..."
+    $BACKEND stop "$BOXER_NAME-$IMAGE_NAME" > /dev/null
     $BACKEND rm "$BOXER_NAME-$IMAGE_NAME" > /dev/null || {
-      echo Failed to remove existing container $BOXER_NAME-$IMAGE_NAME.
-      echo You may need to remove it manually before running boxer.
+      echo "failed to clean up the container: $BOXER_NAME-$IMAGE_NAME."
+      echo "you may need to remove it manually."
       exit(1)
     }
   fi
 
   # create the container
-  echo "creating ephemeral container $BOXER_NAME-$IMAGE_NAME..."
+  #echo "container: $BOXER_NAME-$IMAGE_NAME..."
+  echo "Prepping the match..."
   $BACKEND create -it --mount type=bind,source="$(pwd)",target=/host --workdir /host --hostname "$BOXER_NAME"-"$IMAGE_NAME" --name "$BOXER_NAME"-"$IMAGE_NAME" --entrypoint "/bin/sh" "$IMAGE" > /dev/null || {
-    echo Failed to create the container.
+    echo "Boxer: something went wrong, sorry :("
     exit(1)
   }
   #echo "bind mount created at $(pwd)."
 
   # attach to container
-  #echo "entering $BOXER_NAME-$IMAGE_NAME..."
-  if [ "$IMAGE_NAME" = "debian" ] || [ "$IMAGE_NAME" = "ubuntu" ]; then
-    $BACKEND start -i -a "$BOXER_NAME-$IMAGE_NAME"  # maybe switch to bash since these images include it
-  else
-    $BACKEND start -i -a "$BOXER_NAME-$IMAGE_NAME"
-  fi
+  echo "Fighter $IMAGE_NAME enters the ring!"
+  echo "They're up against $(whoami). Who will win?"
+  echo "Round start..."
+  echo
+  $BACKEND start -i -a "$BOXER_NAME-$IMAGE_NAME"
 
   # remove the container
   $BACKEND rm "$BOXER_NAME-$IMAGE_NAME" > /dev/null || {
-    echo Failed to remove the container. You may need to remove it manually.
+    echo "Failed to remove the container. You may need to remove it manually."
+    exit(1)
   }
-  echo removed $BOXER_NAME-$IMAGE_NAME
-  echo bye!
+  echo
+  echo "$IMAGE_NAME was KO'd!"
+  echo "$(whoami) wins!"
+  echo "See you next time."
 }
